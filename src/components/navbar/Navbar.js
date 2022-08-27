5e001f2b9ef601acbb02ba677eb22e7b81831542
Mohd Tazammul <tazammul1683@gmail.com>
2022-08-28 02:23:40 +0530

    slider added on navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+function Navbar() {
+  return (
+    <div>
+      <div className='slider'>
+        <div>Get 5% OFF upto ₹50 on all UPI Orders</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
