0d73536e8822b55a5fa3273624879fc63ebe2586
Mohd Tazammul <tazammul1683@gmail.com>
2022-08-28 17:00:45 +0530

    dropdown added in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,18 +12,56 @@ function Navbar() {
             <div>
             <img src='https://user-images.githubusercontent.com/90475607/187048302-c4430181-c367-4a23-a913-871774aa2457.png' />
             </div>
-            <div>
-                <p>Shop by Category</p>
-                <p>Shop by Activity</p>
-                <p>Support &amp; warranty</p>
-                <p>Contact us</p>
-                <p>Company</p>
+            <div className="navbar-links">
+              <div className="dropdown">
+                <button className="dropbtn">Shop by category</button>
+                <div className="dropdown-content">
+                  <a href="#">True Wireless</a>
+                  <a href="#">Neckband</a>
+                  <a href="#">Headphones</a>
+                  <a href="#">Wired</a>
+                  <a href="#">Speakers</a>
+                  <a href="#">Smart Watches</a>
+                </div>
+              </div>
+              <div className="dropdown">
+                <button className="dropbtn">Shop by activity</button>
+                <div className="dropdown-content">
+                  <a href="#">Workout</a>
+                  <a href="#">Travel</a>
+                  <a href="#">Work</a>
+                  <a href="#">Entertainment</a>
+                  <a href="#">Audiophile</a>
+                </div>
+              </div>
+              <div className="dropdown">
+                <button className="dropbtn">Support &amp; warranty 
+                </button>
+                <div className="dropdown-content">
+                  <a href="#">Customer Support</a>
+                  <a href="#">Warranty Registation</a>
+                  <a href="#">Service Centres</a>
+                </div>
+              </div> 
+              <div className="dropdown">
+                <button className="dropbtn">Contact us 
+                </button>
+              </div> 
+              <div className="dropdown">
+                <button className="dropbtn">Company
+                </button>
+                <div className="dropdown-content">
+                  <a href="#">About Us</a>
+                  <a href="#">Careers</a>
+                  <a href="#">Why buy from Boultaudio.com</a>
+                </div>
+              </div> 
             </div>
         </div>
         <div className='icons'>
-        <Icon icon="carbon:search" color="white" />
-        <Icon icon="mingcute:user-2-line" color="white" />
-        <Icon icon="mingcute:shopping-bag-2-line" color="white" />
+        <button><Icon icon="carbon:search" color="white" /></button>
+        <button><Icon icon="mingcute:user-2-line" color="white" /></button>
+        <button><Icon icon="mingcute:shopping-bag-2-line" color="white" /></button>
         </div>
       </div>
     </div>
