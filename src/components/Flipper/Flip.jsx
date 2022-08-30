62e821958b344be73da4647c7b11826462532d5f
Mohd Tazammul <tazammul1683@gmail.com>
2022-08-30 20:15:32 +0530

    fliper added

--- src/components/Flipper/Flip.jsx
+++ src/components/Flipper/Flip.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import "./Flip.css"
 const Flip = () => {
+  const flipDivRef = useRef(null);
+  const [flip, setFlip] = useState(true);
+  function flipTheDiv()
+  {
+    if(flip)
+    {
+
+      flipDivRef.current.classList.remove("Card-flip2");
+      flipDivRef.current.classList.add("Card-flip");
+    }
+    else
+    {
+      flipDivRef.current.classList.remove("Card-flip");
+      flipDivRef.current.classList.add("Card-flip2");
+    }
+    setFlip(!flip);
+    console.log(flipDivRef.current)
+  }
   return (
     <div>
         <div className='Container'>
-            <div className='Card'>
+            <div className='Card Card-flip2'  onClick={flipTheDiv} ref={flipDivRef}>
                 <div  className='Front'>
                 Front of the Card
                 </div>
