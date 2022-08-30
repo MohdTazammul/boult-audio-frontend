67da02caacc400eaf437c7bbc69dacb84df85c83
unknown <alakmodak99@gmail.com>
2022-08-30 16:24:49 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import {Route, Routes, BrowserRouter} from "react-router-dom"
 import {useRef} from "react";
+import Login from './components/Login/Login';
 function App() {
   
 const body = useRef(null);
@@ -10,6 +12,12 @@ const body = useRef(null);
     <>
     <Navbar props={body} />
     <div className="App" ref={body}>
+      <BrowserRouter>
+      <Routes>
+        <Route path='login' element={<Login/>}/>
+        {/* <Route path='*' element={<h1>No Route found</h1>}/> */}
+      </Routes>
+      </BrowserRouter>
      {/* <Footer /> */}
     </div>
     </>
