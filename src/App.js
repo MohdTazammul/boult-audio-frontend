b6d70829ced9289f0358d15e9d27c3a51f7d4ad2
Mohd Tazammul <tazammul1683@gmail.com>
2022-08-30 19:47:24 +0530

    merging code

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import { useRef } from "react";
-
 import Login from './components/Login/Login';
-
 import Userprofile from './components/User/Userprofile';
 import { Route, Routes } from 'react-router-dom'
 import TrueWireless from './Pages/TrueWireless';
 import User from './Pages/User';
-
 import Home from "./Pages/Home"
 import NeckbandPage from './Pages/NeckbandPage';
 import HeadphonePage from './Pages/HeadphonePage';
@@ -32,7 +29,7 @@ function App() {
           <Route path='/user' element={<User />} />
           <Route path='/' element={<Home />} />
           <Route path="/Flip" element={<Flip />} />
-          <Route path='/' element={<h1>No Route found</h1>} />
+          <Route path='*' element={<h1>No Route found</h1>} />
         </Routes>
       </div>
     </>
